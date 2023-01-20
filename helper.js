ad695e3f85b1c8374d9ff2e5e45f48e1b3a4740f
artemkuskin <artemkuskin7@gmail.com>
2023-01-20 10:41:12 +0300

    test

--- helper.js
+++ helper.js
@@ -63,7 +63,7 @@ function validateIfImportIsAllowed(pathToCurrentModule, importDefinitionPath, le
          }
          } else {
           const name = importDefinitionPath.split('/')
-           const pathConfigurationOfTargetModule = path.resolve(rootDirectory, name[name.length - 2])
+           const pathConfigurationOfTargetModule = path.resolve(pathToCurrentModule, name[name.length - 2])
            const path1 = pathConfigurationOfTargetModule.split("/")
            const nameFolder = path1[path1.length - 1]
            const targetModuleLevel = new RegExp(`${rootDirectory}\\/(\\w+)`, "g").exec(pathConfigurationOfTargetModule)
@@ -71,7 +71,7 @@ function validateIfImportIsAllowed(pathToCurrentModule, importDefinitionPath, le
           if (currentModuleLevelConfiguration.name !== parentTargetModule.name) {
               if (parentTargetModule.index >=  currentModuleLevelConfiguration.index) {
                 console.log(pathConfigurationOfTargetModule);
-                return `${pathToCurrentModule}`
+                return `${pathConfigurationOfTargetModule}`
               }
             }
          } //else if (currentModuleLevelConfiguration.parents !== configurationOfTargetModule.parents) {
