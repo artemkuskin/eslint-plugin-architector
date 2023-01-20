76d80a658ca7c54aca9961800063374f97587830
artemkuskin <artemkuskin7@gmail.com>
2023-01-20 15:39:02 +0300

    test

--- helper.js
+++ helper.js
@@ -86,7 +86,7 @@ function validateIfImportIsAllowed(pathToCurrentModule, importDefinitionPath, le
           if (moduleTargetLevelFirstName.name === moduleCurrentLevelFirstName.name) {
             //if (moduleCurrentLevelFirstName.index < moduleTargetLevelFirstName.index) {
               //console.log(firstParentModalofTheNonSpecifiedInRules, firstParentCurrentModuleLevelConfiguration);
-              return `${moduleTargetLevelFirstName, moduleCurrentLevelFirstName}`;
+              return `${moduleTargetLevelFirstName.name,moduleTargetLevelFirstName.index,moduleTargetLevelFirstName.parents, moduleCurrentLevelFirstName.name,  moduleCurrentLevelFirstName.nameindex,  moduleCurrentLevelFirstName.parents }`;
             //}
           }
           console.log(moduleTargetLevelFirstName);
