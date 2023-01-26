61d7ae3ffe59ee21fe7c902c8679eec2c5f866bb
artemkuskin <artemkuskin7@gmail.com>
2023-01-26 10:12:42 +0300

    test

--- helper.js
+++ helper.js
@@ -15,86 +15,66 @@ let jsConfigFileContent = require(path.resolve("jsconfig.json"));
 // }
 
 function validateIfImportIsAllowed(pathToCurrentModule, importDefinitionPath, levelsConfiguration, rootDirectory) {
-  // const architectureConfigTree = [];
-  // function getArchitectureConfigurationTree(architectureConfigRules) {
-  //   for (let key in architectureConfigRules) {
-  //     const lastParent = getAllParentThisNode(levelsConfiguration.file, architectureConfigRules[key].level).lastParent;
-  //     const firstParent = getAllParentThisNode(
-  //       levelsConfiguration.file,
-  //       architectureConfigRules[key].level
-  //     ).firstParent;
-  //     architectureConfigTree.push({
-  //       name: architectureConfigRules[key].level,
-  //       index: key,
-  //       parents: lastParent || rootDirectory,
-  //       firstParent: firstParent,
-  //       children: architectureConfigRules[key].children,
-  //     });
-  //     if (architectureConfigRules[key].children.length !== 0) {
-  //       getArchitectureConfigurationTree(architectureConfigRules[key].children);
-  //     }
-  //   }
-  //   return architectureConfigTree;
-  // }
 
-  let parentsAlias = [];
-  console.log(jsConfigFileContent);
-  for (let key in jsConfigFileContent.compilerOptions.paths) {
-    parentsAlias.push({ name: jsConfigFileContent.compilerOptions.paths[key].toString(), key: [key].toString() });
-  }
-  let configurationTreeAlias = [];
-  for (let key in parentsAlias) {
-    configurationTreeAlias.push({
-      key: parentsAlias[key].key
-        .split("/")
-        .splice(0, parentsAlias[key].key.split("/").length - 1)
-        .join("/"),
-      path: path
-        .resolve(
-          parentsAlias[key].name.split("/").splice(0, parentsAlias[key].name.split("/").length).join("/"),
-          rootDirectory
-        )
-        .split("/")
-        .splice(
-          0,
-          path
-            .resolve(
-              parentsAlias[key].name
-                .split("/")
-                .splice(0, parentsAlias[key].name.split("/").length - 1)
-                .join("/"),
-              rootDirectory
-            )
-            .split("/").length - 1
-        )
-        .join("/"),
-    });
-  }
-  let configurationTree = getArchitectureConfigurationTree(
+  // let parentsAlias = [];
+  // console.log(jsConfigFileContent);
+  // for (let key in jsConfigFileContent.compilerOptions.paths) {
+  //   parentsAlias.push({ name: jsConfigFileContent.compilerOptions.paths[key].toString(), key: [key].toString() });
+  // }
+  // let configurationTreeAlias = [];
+  // for (let key in parentsAlias) {
+  //   configurationTreeAlias.push({
+  //     key: parentsAlias[key].key
+  //       .split("/")
+  //       .splice(0, parentsAlias[key].key.split("/").length - 1)
+  //       .join("/"),
+  //     path: path
+  //       .resolve(
+  //         parentsAlias[key].name.split("/").splice(0, parentsAlias[key].name.split("/").length).join("/"),
+  //         rootDirectory
+  //       )
+  //       .split("/")
+  //       .splice(
+  //         0,
+  //         path
+  //           .resolve(
+  //             parentsAlias[key].name
+  //               .split("/")
+  //               .splice(0, parentsAlias[key].name.split("/").length - 1)
+  //               .join("/"),
+  //             rootDirectory
+  //           )
+  //           .split("/").length - 1
+  //       )
+  //       .join("/"),
+  //   });
+  // }
+  const configurationTree = getArchitectureConfigurationTree(
         levelsConfiguration.file,
         levelsConfiguration,
         rootDirectory
       );
-  let keyAlias = importDefinitionPath.split("/")[0];
-  let targetAliasModule = configurationTreeAlias.find((elem) => elem.key === keyAlias); // сделать проверку на сущечтвование
+  const keyAlias = importDefinitionPath.split("/")[0];
+  const configurationTreeAlias = getLevelAlias(rootDirectory);
+  const targetAliasModule = configurationTreeAlias.find((elem) => elem.key === keyAlias); // сделать проверку на сущечтвование
   if (targetAliasModule) {
-    let absolutePathtoTheFileAlias = path.resolve(
+    const absolutePathtoTheFileAlias = path.resolve(
       targetAliasModule.path
         .split("/")
         .slice(0, targetAliasModule.path.split("/").length - 1)
         .join("/"),
       importDefinitionPath
     );
-    let firstParentTargetLevelALias = new RegExp(`${rootDirectory}\\/(\\w+)`, "g").exec(absolutePathtoTheFileAlias); // что импортим
-    let pathToCurrentFile = pathToCurrentModule
+    const firstParentTargetLevelALias = new RegExp(`${rootDirectory}\\/(\\w+)`, "g").exec(absolutePathtoTheFileAlias); // что импортим
+    const pathToCurrentFile = pathToCurrentModule
       .split("/")
       .splice(0, pathToCurrentModule.split("/").length - 1)
       .join("/");
-    let firstParentCurrentLevel = new RegExp(`${rootDirectory}\\/(\\w+)`, "g").exec(pathToCurrentFile); // куда
-    let moduleTargetLevelAliasFirstName = configurationTree.find(
+    const firstParentCurrentLevel = new RegExp(`${rootDirectory}\\/(\\w+)`, "g").exec(pathToCurrentFile); // куда
+    const moduleTargetLevelAliasFirstName = configurationTree.find(
       (elem) => elem.name === firstParentTargetLevelALias[1]
     );
-    let moduleCurentLevelFirstName = configurationTree.find((elem) => elem.name === firstParentCurrentLevel[1]);
+    const moduleCurentLevelFirstName = configurationTree.find((elem) => elem.name === firstParentCurrentLevel[1]);
     console.log(moduleTargetLevelAliasFirstName, moduleCurentLevelFirstName); //mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
     console.log(targetAliasModule);
     if (moduleTargetLevelAliasFirstName.name !== moduleCurentLevelFirstName.name) {
@@ -256,6 +236,42 @@ function getArchitectureConfigurationTree(architectureConfigRules, levelsConfigu
     }
   ).resultarchitectureFree;
   return resultarchitectureFree;
+}
+
+function getLevelAlias(rootDirectory) {
+  const parentsAlias = [];
+  for (let key in jsConfigFileContent.compilerOptions.paths) {
+    parentsAlias.push({ name: jsConfigFileContent.compilerOptions.paths[key].toString(), key: [key].toString() });
+  }
+  const configurationTreeAlias = [];
+  for (let key in parentsAlias) {
+    configurationTreeAlias.push({
+      key: parentsAlias[key].key
+        .split("/")
+        .splice(0, parentsAlias[key].key.split("/").length - 1)
+        .join("/"),
+      path: path
+        .resolve(
+          parentsAlias[key].name.split("/").splice(0, parentsAlias[key].name.split("/").length).join("/"),
+          rootDirectory
+        )
+        .split("/")
+        .splice(
+          0,
+          path
+            .resolve(
+              parentsAlias[key].name
+                .split("/")
+                .splice(0, parentsAlias[key].name.split("/").length - 1)
+                .join("/"),
+              rootDirectory
+            )
+            .split("/").length - 1
+        )
+        .join("/"),
+    });
+  }
+  return configurationTreeAlias;
 }
 
 
