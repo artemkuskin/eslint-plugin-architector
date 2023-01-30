0061ee4b6b5eefba52848f7b01e197dfa3ab9af1
artemkuskin <artemkuskin7@gmail.com>
2023-01-30 13:26:53 +0300

    test

--- src/rules/absolutePathToFile.js
+++ src/rules/absolutePathToFile.js
@@ -2,6 +2,6 @@ const path = require("path");
 module.exports = absolutePathToFile;
 
 function absolutePathToFile(pathToCurrentModule, importDefinitionPath) {
-  return path.resolve(pathToCurrentModule, importDefinitionPath);
+  return path.resolve(pathToCurrentModule, importDefinitionPath).split("\\").join('/');//тут изменил
 }
 
