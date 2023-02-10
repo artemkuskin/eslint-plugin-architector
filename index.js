4b64c773a3ce9f7faf3962a2291965a6e81fced5
artemkuskin <artemkuskin7@gmail.com>
2023-02-10 17:25:47 +0300

    test

--- index.js
+++ index.js
@@ -27,21 +27,22 @@ module.exports.rules = {
       const hierarchy = context.options[0] || DEFAULT_HIERARCHY;
       const componentFolder = context.options[1] || DEFAULT_COMPONENTS_FOLDER;
       return {
-        ImportDeclaration: (node)=> v({
-          node, hierarchy,
-          componentFolder,
-          context
-        }),
-        VariableDeclaration: (node)=>  node.declarations[0].init.arguments[0].value ?  a({
-          node, hierarchy,
-          componentFolder,
-          context
-        }) : undefined
+        ImportDeclaration: (node) =>
+          v({
+            node,
+            hierarchy,
+            componentFolder,
+            context,
+          }),
+        VariableDeclaration: (node) =>
+          a({
+            node,
+            hierarchy,
+            componentFolder,
+            context,
+          }),
       };
-      
-      
     },
-    
   },
 };
 
@@ -49,12 +50,8 @@ function adaptingTheImportPathForLinux(path) {
   return path.split("\\").join("/");
 }
 
-function v ({
-  node, hierarchy,
-  componentFolder,
-  context
-}) {
- // console.log(node);
+function v({ node, hierarchy, componentFolder, context }) {
+  // console.log(node);
   const fn = adaptingTheImportPathForLinux(context.getFilename());
   const nodeValue = adaptingTheImportPathForLinux(node.source.value);
   const params = {
@@ -69,17 +66,15 @@ function v ({
   }
 }
 
-function a ({
-  node, hierarchy,
-  componentFolder,
-  context
-}) {
-  if (node.declarations[0].init.arguments[0].value)  {
+function a({ node, hierarchy, componentFolder, context }) {
+  if (node.declarations[0].init.arguments[0].value) {
     console.log(node.declarations[0].init.arguments[0].value);
+  } else {
+    return undefined;
   }
   // const fn = adaptingTheImportPathForLinux(context.getFilename());
   // const nodeValue = adaptingTheImportPathForLinux(node.declarations[0].init.arguments[0].value);
-  // const params = {  
+  // const params = {
   //   pathToCurrentModule: fn,
   //   importDefinitionPath: nodeValue,
   //   levelsConfiguration: hierarchy,
