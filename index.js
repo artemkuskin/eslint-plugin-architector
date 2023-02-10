779b824204ce1f3b2e3d6d92d54490365021d40c
artemkuskin <artemkuskin7@gmail.com>
2023-02-10 15:42:08 +0300

    test

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ module.exports.rules = {
             levelsConfiguration: hierarchy,
             rootDirectory: componentFolder,
           };
-          console.log(hierarchy);
+          console.log(context);
           const error = validateHierarchy(params);
           if (error) {
             context.report(node, error);
