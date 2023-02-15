b72d1f0c1ffc10d5f50d29f3d09f5ca21cc10543
artemkuskin <artemkuskin7@gmail.com>
2023-02-15 13:46:54 +0300

    test

--- src/helpers/prepareDataForErrorOutput/absolutePathTo.js
+++ src/helpers/prepareDataForErrorOutput/absolutePathTo.js
@@ -3,5 +3,5 @@ const getPathToCurrentFileWithoutExtension = require("../convertPath/pathToCurre
 module.exports = getAbsolutePathTo;
 
 function getAbsolutePathTo(pathToModule, importDefinitionPath) {
-  return absolutePathToFile(getPathToCurrentFileWithoutExtension(pathToModule), importDefinitionPath);
+  return absolutePathToFile(getPathToCurrentFileWithoutExtension(pathToModule), importDefinitionPath).split("\\").join("/");
 }
